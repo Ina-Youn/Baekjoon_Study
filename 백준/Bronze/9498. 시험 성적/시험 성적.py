7b2782073be357605fbdd09910ca4686fa3c2105
Maristella <155051602+Ina-Youn@users.noreply.github.com>
2024-02-06 17:11:22 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+# 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F
+
+a = int(input())
+if 90 <= a :
+    print('A')
+elif 80 <= a :
+    print('B')
+elif 70 <= a :
+    print('C')
+elif 60 <= a :
+    print('D')
+else:
+    print('F')
