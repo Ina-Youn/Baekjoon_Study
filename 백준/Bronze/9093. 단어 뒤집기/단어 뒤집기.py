bc142921f1b286af4b4ed7446dba94312fa1fff4
Maristella <155051602+Ina-Youn@users.noreply.github.com>
2024-03-09 22:30:08 +0900

    Update 단어 뒤집기.py

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,2 +1,12 @@
+### 1st
+
+T=int(input())
+
+for i in range(T):
+    string = list(input().split())
+    for j in string:
+        print(j[::-1],end=' ')
+
+### 2nd
 for _ in range(int(input())):
     print(' '.join([i[::-1] for i in input().split()]))
