157ba52eb4f507ca9f58e0f306294c42a892d597
Maristella <155051602+Ina-Youn@users.noreply.github.com>
2024-03-09 22:07:56 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 736 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,6 +1,2 @@
-T=int(input())
-
-for i in range(T):
-    string = list(input().split())
-    for j in string:
-        print(j[::-1],end=' ')
+for _ in range(int(input())):
+    print(' '.join([i[::-1] for i in input().split()]))
